e70e1492bb18f19868b0699dc3d596f456823e04
lucaspadilha <lucas_padilha@icloud.com>
2020-03-20 15:33:06 +0000

    change the cv to download.

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,9 +1,9 @@
 class DownloadsController < ApplicationController
   def download_cv_en
-    send_file "#{Rails.root}/app/assets/docs/CVLucasENfev2020.pdf", type: "application/pdf", x_sendfile: true
+    send_file "#{Rails.root}/app/assets/docs/CVLucasEN2020.pdf", type: "application/pdf", x_sendfile: true
   end
 
   def download_cv_pt
-    send_file "#{Rails.root}/app/assets/docs/CVLucasPTfev2020.pdf", type: "application/pdf", x_sendfile: true
+    send_file "#{Rails.root}/app/assets/docs/CVLucasPT2020.pdf", type: "application/pdf", x_sendfile: true
   end
 end
